2299034edda164b32d999e1eb94509c5ddad2373
Jakub Kapała <jakub@kapala.xyz>
2026-03-01 18:38:52 +0100

    feat: add basic php Routing class

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
 
+include 'Routing.php';
 
-echo "<h1>Hi there!</h1>";
+// Sanitize the path to prevent XSS attacks
+$path = htmlspecialchars($_SERVER['REQUEST_URI'], ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
+// Extract the path without query parameters
+$path = parse_url($path, PHP_URL_PATH);
+
+Routing::run(ltrim($path, '/'));
